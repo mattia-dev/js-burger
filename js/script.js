43ed52b15b92e0e5b365630a27d668ef3b0dbe7c
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-06-08 01:54:37 +0200

    added js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 var total = document.getElementById('total');
+var button = document.getElementById('calculate-button');
+
+total.innerHTML += 50;
+
+button.addEventListener('click', function () {
+
+    var checkboxes = document.getElementsByClassName('check-ingredients');
+
+    for (var i = 0; i < checkboxes.length; i++) {
+
+        if (checkboxes[i].checked) {
+            total.innerHTML += parseInt(checkboxes[i].value);
+        }
+    }
+
+});
