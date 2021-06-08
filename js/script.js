3b5f09dfa493ea58ef41ff600da396e8e7cb72aa
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-06-08 16:15:33 +0200

    added bootstrap

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
+var burgerName = document.getElementById('burger-name');
 var total = document.getElementById('total');
 var button = document.getElementById('calculate-button');
+var discountComment = document.getElementById('discount-comment');
 
-discounts20 = ["MAY20", "JUNE20", "JULY20", "AUGUST20"];
-discounts30 = ["MAY30", "JUNE30", "JULY30", "AUGUST30"];
+discountCodes20 = ["MAY20", "JUNE20", "JULY20", "AUGUST20"];
+discountCodes30 = ["MAY30", "JUNE30", "JULY30", "AUGUST30"];
 
 var basePrice = 10;
 
+// burgerName.innerHTML = prompt("Come vuoi chiamare il tuo hamburger?");
+
 total.innerHTML = "$ " + basePrice.toFixed(2);
+
+discountComment.innerHTML = "";
 
 button.addEventListener('click', function () {
     var sumPrice = basePrice;
@@ -14,6 +20,7 @@ button.addEventListener('click', function () {
     var promoCode = document.getElementById('discount-coupon');
     var discount20 = 0.2;
     var discount30 = 0.3;
+    var discountCommentText = "";
 
     for (var i = 0; i < checkboxes.length; i++) {
 
@@ -21,22 +28,39 @@ button.addEventListener('click', function () {
             sumPrice += parseFloat(checkboxes[i].value);
         }
 
-        total.innerHTML = "$ " + sumPrice.toFixed(2);
     }
 
-    for (var x = 0; x < discounts20.length; x++) {
-        if (promoCode.value === discounts20[x]) {
+    var flag = false;
+
+    for (var x = 0; x < discountCodes20.length; x++) {
+        
+        if (promoCode.value === discountCodes20[x]) {
             sumPrice -= sumPrice * discount20;
+            flag = true;
         }  
 
-        total.innerHTML = "$ " + sumPrice.toFixed(2);
     }
 
-    for (var y = 0; y < discounts30.length; y++) {
-        if (promoCode.value === discounts30[y]) {
+    for (var y = 0; y < discountCodes30.length; y++) {
+        
+        if (promoCode.value === discountCodes30[y]) {
             sumPrice -=  (sumPrice * discount30);
+            flag = true;
         }
 
-        total.innerHTML = "$ " + sumPrice.toFixed(2);
     }
+
+    if (promoCode.value.length > 0) {
+        if (flag) {
+            discountCommentText = "Il codice sconto è stato inserito correttamente!";
+            discountComment.style.color = "green";
+        } else {
+            discountCommentText = "Il codice sconto inserito non è valido o è scaduto.";
+            discountComment.style.color = "red";
+        }
+    }
+
+    total.innerHTML = "$ " + sumPrice.toFixed(2);
+
+    discountComment.innerHTML = discountCommentText;
 });
